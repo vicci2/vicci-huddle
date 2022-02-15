31fbe14f5c4641c7d4a09389b835d6297289df53
Vicci2 <vicci2regia@gmail.com>
2022-02-16 00:08:19 +0300

    night commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from flask import Flask,flash, redirect, render_template, request, url_for,session
 import psycopg2
 from flask_wtf import FlaskForm
@@ -10,8 +11,8 @@ app =Flask(__name__)
 app.secret_key="123secrete kye"
 # app.permanent_session_lifetime=timedelta(minutes=10)
 try:
-    conn=psycopg2.connect("dbname='huddle' user='postgres' host='localhost' password='vicciSQL'")
-    # conn=psycopg2.connect("dbname='d4augsl57bdont' user='epbnudknwnktpp' port='5432' host='ec2-34-199-15-136.compute-1.amazonaws.com' password='863b87226bd0eafa17748cc14d4bdcb0af464d40c35796f8bf38cce8dc378153'")
+    # conn=psycopg2.connect("dbname='huddle' user='postgres' host='localhost' password='vicciSQL'")
+    conn=psycopg2.connect("dbname='dk28dn22dcnb2' user='lwbdaaftujgejr' port='5432' host='ec2-54-194-147-61.eu-west-1.compute.amazonaws.com' password='cda07fc755061b7a120e7fa2d8f6144dc6268aa98131ef59eeefe2fa3d32da00'")
     print("Successful D.B Connection")
 except:
     print("Connection error!")
@@ -31,16 +32,12 @@ def reg():
         user_name=request.form["user_name"]
         age=request.form["age"]
         password=request.form["password"]
-        # cpassword=request.form["cpassword"]
-        country=request.form["country"]
-        city=request.form["city"]
-        street=request.form["street"]
+        # cpassword=request.form["cpassword"]     
         phone_number=request.form["phone_number"]
         second_phone_number=request.form["second_phone_number"]
-        email_address=request.form["email_address"]
-        
-        query="INSERT INTO public.login (user_name, age, password, country, city, street, phone_number, second_phone_number, email_address) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-        row=(user_name,age,password,country,city,street,phone_number,second_phone_number,email_address)
+        email_address=request.form["email_address"] 
+        query="INSERT INTO public.login (user_name, age, password,phone_number, second_phone_number, email_address) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+        row=(user_name,age,password,phone_number,second_phone_number,email_address)
         cur.execute(query,row)
         conn.commit()
         flash("Registration successful!")
@@ -49,22 +46,25 @@ def reg():
         flash("Please enter user details.")
         return render_template("huddlereg.html")
 
-@app.route('/log_in',methods=["post"])
+@app.route('/log_in',methods=["GET","POST"])
 def log_in():
     cur=conn.cursor()
-    user_name=request.form["user_name"]
+    username=request.form["username"]
     password=request.form["password"]
-    query=""
+    query="SELECT user_name,password FROM public.login; "
     cur.execute(query)
-    if user_name==user_name:
-        print("hbbvhjv")
-    else:
-       print("pasdjskjfvf")
-    data=(user_name,password)
-    
-    conn.commit()
-    return redirect(url_for('chat'))
-    
+    abc=cur.fetchall()
+    print(abc)
+    for i in abc:
+        if username==i[0] and password==i[1]:
+            # print("hbbvhjv")
+            flash("Login Successful!",messages)
+            return redirect(url_for('chat'))
+        else:
+            # print("pasdjskjfvf")  
+            flash("Invalid User Details.Try Again!")
+            return redirect(url_for('log_in'))
+    return redirect(url_for('ims'))
 
 @app.route('/chatroom')
 def chat():
