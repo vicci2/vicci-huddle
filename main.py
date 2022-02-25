ce1e98e83adca122579b641cbff180134c29f39e
Vicci2 <vicci2regia@gmail.com>
2022-02-25 13:39:05 +0300

    first commit

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app =Flask(__name__)
 app.secret_key="123secrete kye"
 # app.permanent_session_lifetime=timedelta(minutes=10)
 try:
-    # conn=psycopg2.connect("dbname='huddle' user='postgres' host='localhost' password='vicciSQL'")
-    conn=psycopg2.connect("dbname='dk28dn22dcnb2' user='lwbdaaftujgejr' port='5432' host='ec2-54-194-147-61.eu-west-1.compute.amazonaws.com' password='cda07fc755061b7a120e7fa2d8f6144dc6268aa98131ef59eeefe2fa3d32da00'")
+    conn=psycopg2.connect("dbname='huddle' user='postgres' host='localhost' password='vicciSQL'")
+    #conn=psycopg2.connect("dbname='dk28dn22dcnb2' user='lwbdaaftujgejr' port='5432' host='ec2-54-194-147-61.eu-west-1.compute.amazonaws.com' password='cda07fc755061b7a120e7fa2d8f6144dc6268aa98131ef59eeefe2fa3d32da00'")
     print("Successful D.B Connection")
 except:
     print("Connection error!")
@@ -49,26 +49,46 @@ def reg():
 @app.route('/log_in',methods=["GET","POST"])
 def log_in():
     cur=conn.cursor()
-    username=request.form["username"]
+    user_name=request.form["user_name"]
     password=request.form["password"]
     query="SELECT user_name,password FROM public.login; "
-    cur.execute(query)
+    row=(user_name,password)
+    cur.execute(query,row)
     abc=cur.fetchall()
     print(abc)
     for i in abc:
-        if username==i[0] and password==i[1]:
-            # print("hbbvhjv")
-            flash("Login Successful!",messages)
+        if user_name==i[0] and password==i[1]:
+            print(i[0],i[1])
+            flash("Login Successful!")
             return redirect(url_for('chat'))
         else:
-            # print("pasdjskjfvf")  
+            print(i[0],i[1])  
             flash("Invalid User Details.Try Again!")
             return redirect(url_for('log_in'))
-    return redirect(url_for('ims'))
 
 @app.route('/chatroom')
 def chat():
     return render_template("huddlechat.html")
+
+# @app.route('/loginer',methods=["GET","POST"])
+# def login():
+#     cur=conn.cursor()
+#     usernme=request.form["username"]
+#     password=request.form["password"]
+#     query="SELECT user_name,password FROM public.login; "
+#     cur.execute(query)
+#     abc=cur.fetchall()
+#     print(abc)
+#     for i in abc:
+#         if usernme==i[0] and password==i[1]:
+#             print(i[0],i[1])
+#             flash("User verified!")
+#             return redirect(url_for('coments'))
+#         else:
+#             print(i[0],i[1])  
+#             flash("Invalid User Details.Try Again!")
+#             return redirect(url_for('login'))
+#     return redirect(url_for('ims'))
 
 @app.route('/comments')
 def coments():
