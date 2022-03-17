5ead1f5b96094a74269cbe462455ff92172295b4
Vicci2 <vicci2regia@gmail.com>
2022-03-17 17:04:59 +0300

    first commit

--- main.py
+++ main.py
@@ -5,24 +5,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField
 from wtforms.validators import InputRequired,DataRequired, EqualTo, Length
 from flask_login import UserMixin, login_user, login_manager,login_required,logout_user, current_user
-# from datetime import datetime
+
 
 app =Flask(__name__)
 app.secret_key="123secrete kye"
 # app.permanent_session_lifetime=timedelta(minutes=10)
 try:
-    conn=psycopg2.connect("dbname='huddle' user='postgres' host='localhost' password='vicciSQL'")
-    #conn=psycopg2.connect("dbname='dk28dn22dcnb2' user='lwbdaaftujgejr' port='5432' host='ec2-54-194-147-61.eu-west-1.compute.amazonaws.com' password='cda07fc755061b7a120e7fa2d8f6144dc6268aa98131ef59eeefe2fa3d32da00'")
+    # conn=psycopg2.connect("dbname='huddle' user='postgres' host='localhost' password='vicciSQL'")
+    conn=psycopg2.connect("dbname='dk28dn22dcnb2' user='lwbdaaftujgejr' port='5432' host='ec2-54-194-147-61.eu-west-1.compute.amazonaws.com' password='cda07fc755061b7a120e7fa2d8f6144dc6268aa98131ef59eeefe2fa3d32da00'")
     print("Successful D.B Connection")
 except:
     print("Connection error!")
 
-# class LoginForm(FlaskForm):
-#     username=StringField('username',validators=[InputRequired(),Length(min=4, max=80)] )
-#     username=PasswordField('password',validators=[InputRequired(),Length(min=4, max=80)] )
 
 @app.route('/')
-def ims():
+def home():
     return render_template("index.html")
 
 @app.route('/register',methods=["GET","POST"])
@@ -41,7 +38,7 @@ def reg():
         cur.execute(query,row)
         conn.commit()
         flash("Registration successful!")
-        return redirect(url_for('ims'))
+        return redirect(url_for('home'))
     else:        
         flash("Please enter user details.")
         return render_template("huddlereg.html")
@@ -49,49 +46,35 @@ def reg():
 @app.route('/log_in',methods=["GET","POST"])
 def log_in():
     cur=conn.cursor()
-    user_name=request.form["user_name"]
-    password=request.form["password"]
-    query="SELECT user_name,password FROM public.login; "
-    row=(user_name,password)
-    cur.execute(query,row)
-    abc=cur.fetchall()
-    print(abc)
-    for i in abc:
-        if user_name==i[0] and password==i[1]:
-            print(i[0],i[1])
-            flash("Login Successful!")
-            return redirect(url_for('chat'))
-        else:
-            print(i[0],i[1])  
-            flash("Invalid User Details.Try Again!")
-            return redirect(url_for('log_in'))
+    if request.method=='POST':
+        user_name=request.form["user_name"]
+        password=request.form["password"]
+        query="SELECT user_name,password FROM public.login; "
+        row=(user_name,password)
+        cur.execute(query,row)
+        abc=cur.fetchall()
+        print(abc)
+        for i in abc:
+            if user_name==i[0] and password==i[1]:
+                print(i[0],i[1])
+                flash("Login Successful!")
+                return redirect(url_for('chat',name=row[0]))
+      
+    flash("Invalid User Details.Try Again!")
+    return redirect(url_for('home'))    
 
 @app.route('/chatroom')
 def chat():
     return render_template("huddlechat.html")
 
-# @app.route('/loginer',methods=["GET","POST"])
-# def login():
-#     cur=conn.cursor()
-#     usernme=request.form["username"]
-#     password=request.form["password"]
-#     query="SELECT user_name,password FROM public.login; "
-#     cur.execute(query)
-#     abc=cur.fetchall()
-#     print(abc)
-#     for i in abc:
-#         if usernme==i[0] and password==i[1]:
-#             print(i[0],i[1])
-#             flash("User verified!")
-#             return redirect(url_for('coments'))
-#         else:
-#             print(i[0],i[1])  
-#             flash("Invalid User Details.Try Again!")
-#             return redirect(url_for('login'))
-#     return redirect(url_for('ims'))
+@app.route('/coment',methods=["GET","POST"])
+def coment():
+     
+    return redirect(url_for('comments'))
 
 @app.route('/comments')
-def coments():
+def comments():
     return render_template("chatrrom.html")
+
 if __name__ == '__main__':    
     app.run(debug=True)
